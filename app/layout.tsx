38226692714c65b7ca458bc17ad02c0c72b28600
Gautam Singh <gautamsingh12122003@gmail.com>
2026-08-10 17:20:54 +0530

    feat: Add shadcn and theme support with Tailwind CSS integration. Introduced global styles and layout adjustments, including a mode toggle for light/dark themes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Public_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-providers";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const publicSans = Public_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -21,9 +26,26 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
         <html
             lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+            suppressHydrationWarning
+            className={cn(
+                "h-full",
+                "antialiased",
+                geistSans.variable,
+                geistMono.variable,
+                "font-sans",
+                publicSans.variable,
+            )}
         >
-            <body className="min-h-full flex flex-col">{children}</body>
+            <body className="min-h-full flex flex-col">
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <TooltipProvider>{children}</TooltipProvider>
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
